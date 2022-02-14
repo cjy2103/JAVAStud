5493f9aa4cec3fa281c52208d6e2bc64f5776802
cjy2103 <cjy2103@naver.com>
2022-02-15 03:06:20 +0900

    Generic 설명 클래스, 메소드 부분

--- blogPosting/Generic/src/ClassName.java
+++ blogPosting/Generic/src/ClassName.java
@@ -1,2 +1,24 @@
 public class ClassName<T,K>{
+    private T param;
+    private K value;
+
+    public T getParam() {
+        return param;
+    }
+
+    public void setParam(T param) {
+        this.param = param;
+    }
+
+    public K getValue() {
+        return value;
+    }
+
+    public void setValue(K value) {
+        this.value = value;
+    }
+
+    public <T> T generic(T o){
+        return o;
+    }
 }
--- blogPosting/Generic/src/Main.java
+++ blogPosting/Generic/src/Main.java
@@ -1,7 +1,27 @@
-
-
 public class Main {
     public static void main(String[] args) {
+        ClassName<String, Integer> className = new ClassName<>();
+        ClassName<Integer,Double> className2 = new ClassName<>();
+
+        className.setParam("°ª");
+        className.setValue(10);
+        className2.setValue(20.0);
+        className2.setParam(20);
+
+        System.out.println(className.getParam());
+        System.out.println(className.getParam().getClass().getName());
+        System.out.println(className.getValue());
+        System.out.println(className.getValue().getClass().getName());
+
+        System.out.println(className2.getParam());
+        System.out.println(className2.getParam().getClass().getName());
+        System.out.println(className2.getValue());
+        System.out.println(className2.getValue().getClass().getName());
+
+        System.out.println(className.generic("Hello").getClass().getName());
+        System.out.println(className.generic(3).getClass().getName());
+        System.out.println(className.generic(className2).getClass().getName());
+
 
     }
 }
