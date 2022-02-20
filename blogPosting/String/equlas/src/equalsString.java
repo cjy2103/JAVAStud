22bd079fcfc2b48696d64029741f350980b650cc
cjy2103 <cjy2103@naver.com>
2022-02-21 00:41:16 +0900

    == equals 차이 포스팅

--- blogPosting/String/equlas/src/equalsString.java
+++ blogPosting/String/equlas/src/equalsString.java
@@ -1,5 +1,21 @@
 public class equalsString {
     public static void main(String[] args) {
+        String str  = "abc"; // 리터럴을 이용한 변수 초기화
+        String str2 = "abc";
 
+        String str3 = new String("abc"); // new 연산자를 이용한 변수 초기화
+        String str4 = new String("abc");
+
+        System.out.println(str == str2);
+        System.out.println(str.equals(str2));
+
+        System.out.println(str == str3);
+        System.out.println(str.equals(str3));
+
+        System.out.println(str == str4);
+        System.out.println(str.equals(str4));
+
+        System.out.println(str3 == str4);
+        System.out.println(str3.equals(str4));
     }
 }
