606fb98654456e8bd8e05fb362c3208322b15d17
cjy2103 <cjy2103@naver.com>
2022-03-12 22:17:59 +0900

    Method Function 차이 설명 포스팅 완

---
+++ blogPosting/Method/src/Test.java
@@ -0,0 +1,12 @@
+
+//public int numberAdd(int a, int b){
+//        return a+b;
+//        }
+
+public class Test {
+    public int numberPlus(int a, int b){
+        return a+b;
+    }
+}
+
+
---
+++ blogPosting/TypeFind/src/TypeTest.java
@@ -0,0 +1,10 @@
+public class TypeTest {
+
+    public int findTypeInt(){
+        int a = 1;
+        int b = 2;
+        return a+b;
+    }
+
+
+}
