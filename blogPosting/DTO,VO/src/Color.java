3393239eb94d5d2657bf2c23c2b4de81fed426be
jy.choi <cjy2103@naver.com>
2022-03-15 10:33:13 +0900

    DTO,VO 포스팅 완

---
+++ blogPosting/DTO,VO/src/Color.java
@@ -0,0 +1,13 @@
+
+
+public class Color {
+    private final String color;
+
+    public Color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
