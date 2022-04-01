17ec42a7143f5f4f124b34edc28d10e77cc848da
jy.choi <cjy2103@naver.com>
2022-04-01 17:52:42 +0900

    StringBuilder 포스팅 완

---
+++ blogPosting/String/StringBuilder/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder();
+        str.append("안녕하세요");
+        str.append(" 문자열 추가");
+
+        System.out.println(str);
+
+        System.out.println(str.insert(4,"값 삽입"));
+
+        System.out.println(str.delete(4,8));
+
+        System.out.println(str.indexOf("자"));
+
+        System.out.println(str.substring(3,8));
+
+        System.out.println(str.length());
+
+        System.out.println(str.replace(4,7,"이걸로 변경"));
+
+        System.out.println(str.reverse());
+    }
+
+}
