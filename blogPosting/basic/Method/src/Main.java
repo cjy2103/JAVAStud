b8b2da38beaf5239591f4fcba3ef7e868782c271
cjy2103 <cjy2103@naver.com>
2022-05-15 23:32:03 +0900

    TODO List 갱신

--- blogPosting/basic/Method/src/Main.java
+++ blogPosting/basic/Method/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Test test = new Test();
+        int sum = test.numberPlus(3,5);
+        System.out.println(sum);
+        test.minus(5,2);
     }
 
 }
--- blogPosting/basic/Method/src/Test.java
+++ blogPosting/basic/Method/src/Test.java
@@ -4,9 +4,16 @@
 //        }
 
 public class Test {
+
     public int numberPlus(int a, int b){
         return a+b;
     }
+
+    public void minus(int a, int b){
+        System.out.println(a-b);
+    }
+
+
 }
 
 
