572be05f63342ca73a16d43fa44cca1e0a2d55f7
jy.choi <cjy2103@naver.com>
2022-05-17 17:42:15 +0900

    객체와 인스턴스 포스팅 완

---
+++ blogPosting/basic/Object,Instance/src/Frame.java
@@ -0,0 +1,14 @@
+
+// 붕어빵 틀 
+public class Frame {
+    
+    private String name; // 붕어빵 이름
+
+    public Frame(String name) {
+        this.name = name;
+    }
+
+    public void printName(){
+        System.out.println(name);
+    }
+}
---
+++ blogPosting/basic/Object,Instance/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Frame redBean; // 객체 ( 붕어빵 )
+        // 인스턴스화
+        redBean = new Frame("팥붕어빵"); // readBean은 Frame 클래스의 '인스턴스' (붕어빵 종류)
+        redBean.printName();
+
+        Frame puff; // 객체 ( 붕어빵 )
+        //인스턴스화
+        puff = new Frame("슈크림 붕어빵"); // puff는 Frame 클래스의 '인스턴스' (붕어빵 종류)
+        puff.printName();
+
+        Frame choco; // 객체 (붕어빵)
+        //인스턴스화
+        choco = new Frame("초코 붕어빵"); // choco는 Frame 클래스의 '인스턴스' (붕어빵 종류)
+        choco.printName();
+
+
+    }
+}
