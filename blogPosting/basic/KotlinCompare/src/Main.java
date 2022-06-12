3735c92c8d8d7cf4098a2dc30e605e5e4fbbaa98
cjy2103 <cjy2103@naver.com>
2022-06-12 21:03:28 +0900

    코틀린 비교용

---
+++ blogPosting/basic/KotlinCompare/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("값을 입력하세요");
+
+        int num = sc.nextInt();
+
+        System.out.println("값은 :" + num);
+
+
+    }
+}
