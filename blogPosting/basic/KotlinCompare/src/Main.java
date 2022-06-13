0909f2dfa63ec9af018a99dc4945cb721a60728e
jy.choi <cjy2103@naver.com>
2022-06-13 14:08:10 +0900

    코틀린 비교

--- blogPosting/basic/KotlinCompare/src/Main.java
+++ blogPosting/basic/KotlinCompare/src/Main.java
@@ -4,11 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("값을 입력하세요");
+        System.out.println("1 ~ 3 사이의 값을 입력하세요");
 
         int num = sc.nextInt();
-
-        System.out.println("값은 :" + num);
+        
+        
+        switch (num) {
+            case 1 :
+                System.out.println("1입력함");
+                break;
+            case 2 :
+                System.out.println("2입력함");
+                break;
+            case 3 :
+                System.out.println("3입력함");
+                break;
+            default:
+                System.out.println("`1 ~ 3 사이의 숫자를 입력하세요`");
+                break;
+                
+        }
 
 
     }
