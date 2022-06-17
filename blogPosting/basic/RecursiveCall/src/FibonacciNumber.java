d0b96b4c8250c3983244f51341188ec3f3090b85
jy.choi <cjy2103@naver.com>
2022-06-17 11:52:23 +0900

    재귀 포스팅 완

--- blogPosting/basic/RecursiveCall/src/FibonacciNumber.java
+++ blogPosting/basic/RecursiveCall/src/FibonacciNumber.java
@@ -1,9 +1,22 @@
 public class FibonacciNumber {
-    public void recursiveFib(){
-        
+    public int recursiveFib(int n){
+        if(n<=1) return n;
+        return recursiveFib(n-1) + recursiveFib(n-2);
     }
 
-    public void normalFib(){
-
+    public int normalFib(int n){
+        int reuslt = 0;
+        int first = 1;
+        int second = 1;
+        if(n==1 || n==2){
+            return 1;
+        } else {
+            for(int i=3; i<=n;i++){
+                reuslt = first + second;
+                first = second;
+                second = reuslt;
+            }
+            return reuslt;
+        }
     }
 }
--- blogPosting/basic/RecursiveCall/src/Main.java
+++ blogPosting/basic/RecursiveCall/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +6,8 @@ public class Main {
         System.out.println(factorial.recursiveFac(5));
         System.out.println(factorial.normalFac(5));
 
-
+        FibonacciNumber fibonacciNumber = new FibonacciNumber();
+        System.out.println(fibonacciNumber.recursiveFib(5));
+        System.out.println(fibonacciNumber.normalFib(5));
     }
 }
