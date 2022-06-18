7361b0ced9d19c60ff7354dda934d9ffd22bd9d8
cjy2103 <cjy2103@naver.com>
2022-06-18 22:19:52 +0900

    갱신

--- blogPosting/basic/NullCheck/src/Main.java
+++ blogPosting/basic/NullCheck/src/Main.java
@@ -2,10 +2,17 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-//        Test test = null;
-        Test test = new Test();
+        Test test = null;
+//        Test test = new Test();
+        String temp = "";
 
-        if(test == null){
+        if(temp.isEmpty()){
+            System.out.println("널임");
+        } else {
+            System.out.println("널아님");
+        }
+
+        if(temp.isEmpty()){
             System.out.println("널임");
         } else {
             test.test();
