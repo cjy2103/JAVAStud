d5210d7b4471465e5b4a7006722a75a5d590d194
jy.choi <cjy2103@naver.com>
2022-06-21 11:44:37 +0900

    반복문 탈출 포스팅 완

---
+++ blogPosting/Loop/Break,Continue/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=0; i<5;i++){
+            if(i==3){
+               System.out.println("Loop문 탈출");
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+    }
+}
+
+
+
