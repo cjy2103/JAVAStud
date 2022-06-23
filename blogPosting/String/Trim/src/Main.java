a0bc254e0e84e3c603c76408fbf5b1d3edb6c601
jy.choi <cjy2103@naver.com>
2022-06-23 10:16:36 +0900

    Trim 포스팅 완

---
+++ blogPosting/String/Trim/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = " 문자열 공백제거 trim 테스트 ";
+        
+        System.out.println(str);
+
+        str = str.trim();
+        System.out.println(str);
+    }
+}
