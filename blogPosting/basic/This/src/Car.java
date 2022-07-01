693af19075b9ada7c855e14d90e357874ac3b4ed
jy.choi <cjy2103@naver.com>
2022-07-01 10:11:19 +0900

    자바 this 의미 포스팅 완

---
+++ blogPosting/basic/This/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    // 필드
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
