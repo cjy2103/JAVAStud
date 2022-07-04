fb0ee9b8f1645d081e56ec1cf0daae7aa222f17e
jy.choi <cjy2103@naver.com>
2022-07-04 12:30:33 +0900

    Kotlin Comapnion Objcet 비교 포스팅중

--- blogPosting/KotlinCompare/Static/src/Main.java
+++ blogPosting/KotlinCompare/Static/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Number.plus(5,6);
+
+        Person person = new Person("철수");
+        person.print();
     }
 }
---
+++ blogPosting/KotlinCompare/Static/src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void print(){
+        System.out.println("내 이름은 "+name+"입니다.");
+    }
+}
