17a3dd5fe867b1d92eadfe15d1be5a984219e913
jy.choi <cjy2103@naver.com>
2022-07-11 14:30:57 +0900

    비트 이동 연산자 테스트

---
+++ blogPosting/basic/Shift/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2 << 4 = " + (2<<4));
+        System.out.println("32 >> 2 = " + (32>>2));
+        System.out.println("-32 >> 3 = " + (-32>>3));
+        System.out.println("-32 >>> 2 = " + (-32>>>2));
+    }
+}
