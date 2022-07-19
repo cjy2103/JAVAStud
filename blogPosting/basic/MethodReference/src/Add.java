37916611fca85d75ae4cfd0a964b16aeeecd8241
jy.choi <cjy2103@naver.com>
2022-07-19 11:16:51 +0900

    더블콜론 포스팅 완

---
+++ blogPosting/basic/MethodReference/src/Add.java
@@ -0,0 +1,3 @@
+public interface Add {
+    public int plus(int a, int b);
+}
--- blogPosting/basic/MethodReference/src/Main.java
+++ blogPosting/basic/MethodReference/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Test test = new Test();
+        test.setA(20);
+        test.setB(50);
+
+//        test.plus((a,b)->{
+//            return a+b;
+//        });
+
+        test.plus(Integer::sum);
+
     }
 }
+
+
