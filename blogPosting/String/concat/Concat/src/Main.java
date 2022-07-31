8b3f747647fd8ef317b1c7b99b07f3ef276750ee
cjy2103 <cjy2103@naver.com>
2022-07-31 19:40:37 +0900

    concat, append 포스팅 완

--- blogPosting/String/concat/Concat/src/Main.java
+++ blogPosting/String/concat/Concat/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
+        StringTest stringTest = new StringTest();
+//        stringTest.concatTest();
+        stringTest.appendTest();
     }
 }
---
+++ blogPosting/String/concat/Concat/src/StringTest.java
@@ -0,0 +1,16 @@
+public class StringTest {
+    public void concatTest(){
+        String a = "String";
+        String b = " Test";
+
+        System.out.println(a.concat(b));
+    }
+
+    public void appendTest(){
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" String");
+        sb.append(" Test");
+        System.out.println(sb);
+        
+    }
+}
