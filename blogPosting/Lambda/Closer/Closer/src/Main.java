11df8e6b5bb52c06bfc494ea35d2b604279aff09
jy.choi <cjy2103@naver.com>
2022-09-01 11:04:55 +0900

    Java 클로저 포스팅 완

--- blogPosting/Lambda/Closer/Closer/src/Main.java
+++ blogPosting/Lambda/Closer/Closer/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        Number num = new Number(3,5);
 
+        int result;
+        int c = 10;
+
+        Number num = new Number();
+
+        result = num.add(5, 10, (a, b) -> {
+           // result = a+b; // 불가능
+            return a+b+c;
+        });
+
+        System.out.println(result);
     }
 }
--- blogPosting/Lambda/Closer/Closer/src/Number.java
+++ blogPosting/Lambda/Closer/Closer/src/Number.java
@@ -1,15 +1,8 @@
 public class Number {
 
-    private int a;
-    private int b;
-
-    public Number(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public int add(Add add){
-        int result = add.plus(a,b);
-        return result;
+    public int add(int a, int b, Add add){
+        return add.plus(a,b);
     }
 }
+
+
