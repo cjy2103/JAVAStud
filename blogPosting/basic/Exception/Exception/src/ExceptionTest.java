478c825dc9577e3cf9b76af72a8f8a17b85358b1
jy.choi <cjy2103@naver.com>
2022-09-16 14:49:03 +0900

    try/catch 포스팅 완

--- blogPosting/basic/Exception/Exception/src/ExceptionTest.java
+++ blogPosting/basic/Exception/Exception/src/ExceptionTest.java
@@ -1,3 +1,32 @@
 public class ExceptionTest {
 
+    public void npeTest() {
+        String str = null;
+
+        try {
+            if (str.isEmpty()) {
+                System.out.println("공백");
+            } else {
+                System.out.println(str + "출력");
+            }
+        } catch (NullPointerException e){
+            System.out.println("NPE 발생, 상세내용: "+e);
+        } finally {
+            System.out.println("모르겠고 일단 실행시켜");
+        }
+    }
+
+    public void npeTest2() {
+        try{
+            test();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void test() throws NullPointerException{
+        System.out.println("이곳에서 예외를 처리하겠다.");
+    }
+
+
 }
--- blogPosting/basic/Exception/Exception/src/Main.java
+++ blogPosting/basic/Exception/Exception/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
+        ExceptionTest exceptionTest = new ExceptionTest();
+        exceptionTest.npeTest();
 
+        exceptionTest.npeTest2();
     }
 }
