80f91b5a4f142da4f099035a69bf90ce5f43b7bd
cjy2103 <cjy2103@naver.com>
2022-10-17 01:04:52 +0900

    배열복사 완

--- blogPosting/Array/basic/ArrayCopy/ArrayCopy/src/ArrayCopy.java
+++ blogPosting/Array/basic/ArrayCopy/ArrayCopy/src/ArrayCopy.java
@@ -1,6 +1,13 @@
-public class ArrayCopy {
+import java.util.Arrays;
 
-    public void arrayCopy(){
-        
+public class ArrayCopy {
+    public int[] arrayCopy(int[] originArr){
+        int[] copyArr = new int[originArr.length];
+
+        for(int i=0;i<originArr.length;i++){
+            copyArr[i] = originArr[i];
+        }
+        return copyArr;
     }
+
 }
--- blogPosting/Array/basic/ArrayCopy/ArrayCopy/src/Main.java
+++ blogPosting/Array/basic/ArrayCopy/ArrayCopy/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        int[] originArr = {1,2,3,4,5};
+        int[] copyArr = originArr.clone();
+
+//        ArrayCopy arrayCopy = new ArrayCopy();
+//
+//        copyArr = arrayCopy.arrayCopy(originArr);
+//
+//        System.arraycopy(originArr, 0, copyArr, 0, originArr.length);
+
+        System.out.println(Arrays.toString(copyArr));
+
+
 
     }
 }
