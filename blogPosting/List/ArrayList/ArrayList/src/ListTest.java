21c86568eb21750be0acd51568e9e911d190d0b8
jy.choi <cjy2103@naver.com>
2022-10-17 17:28:26 +0900

    리스트 포스팅 완

---
+++ blogPosting/List/ArrayList/ArrayList/src/ListTest.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ListTest {
+
+    public void listTest(){
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+        System.out.println(arrayList);
+
+    }
+}
---
+++ blogPosting/List/ArrayList/ArrayList/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(10);
+        arrayList.add(6);
+        arrayList.add(5);
+
+        System.out.println(arrayList);
+
+        arrayList.sort(Comparator.naturalOrder());
+
+        System.out.println(arrayList);
+
+        arrayList.sort(Comparator.reverseOrder());
+
+        System.out.println(arrayList);
+    }
+}
