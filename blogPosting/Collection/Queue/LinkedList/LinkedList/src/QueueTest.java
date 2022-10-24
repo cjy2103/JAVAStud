eafa6df91764bb0a2e11fb64afab354e5708ad5b
jy.choi <cjy2103@naver.com>
2022-10-24 13:42:06 +0900

    큐 포스팅 완

--- blogPosting/Collection/Queue/LinkedList/LinkedList/src/QueueTest.java
+++ blogPosting/Collection/Queue/LinkedList/LinkedList/src/QueueTest.java
@@ -1,2 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueTest {
+
+    public void queueTest(){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.offer(4);
+
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue);
+
+    }
 }
