b4925f3f0cfa1e206a1ad9c9fca5e909bb153673
jy.choi <cjy2103@naver.com>
2022-11-02 18:00:31 +0900

    length, length() 차이 포스팅중

---
+++ blogPosting/Array/basic/length/length/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+    public void test(){
+        int[] array = {1,2,3,4,5};
+
+        for (int i=0;i<array.length;i++){
+            System.out.println("요소 : "+array[i]);
+        }
+
+        String str = "Hello";
+        for(int i=0;i<str.length();i++){
+            System.out.println("요소:");
+        }
+
+
+    }
+}
