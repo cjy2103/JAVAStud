fde188a08631c50de363b151605ab15164678209
jy.choi <cjy2103@naver.com>
2022-11-07 03:48:38 +0900

    저장

--- blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/SelectedSort.java
+++ blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/SelectedSort.java
@@ -1,6 +1,12 @@
 public class SelectedSort {
 
-    public void sort(){
+    public void sort(int[] array,int size){
+        int minIndex;
+        for(int i=0;i<array.length;i++){
+            minIndex = i;
+            for(int j=i;j<size;j++){
 
+            }
+        }
     }
 }
