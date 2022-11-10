05e543d8c195b14a9b559e6932145594d60b1ad8
jy.choi <cjy2103@naver.com>
2022-11-10 14:04:00 +0900

    선택정렬 포스팅 완

--- blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/Main.java
+++ blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] array = {7,6,2,4,1,5,8,3};
+        SelectedSort sort = new SelectedSort();
+        array = sort.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
--- blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/SelectedSort.java
+++ blogPosting/Algorithm/Sort/SelectedSort/SelectedSort/src/SelectedSort.java
@@ -1,12 +1,25 @@
 public class SelectedSort {
-
-    public void sort(int[] array,int size){
-        int minIndex;
-        for(int i=0;i<array.length;i++){
+    /**
+     * @DESC: minIndex : 최소값 있는 위치
+     * temp : 최소값 저장하기 위한 공간
+     * @param array
+     * @return
+     */
+    public int[] sort(int[] array){
+        int minIndex, temp;
+        //마지막 회차는 할 필요 없음
+        for(int i=0;i<array.length-1;i++){
             minIndex = i;
-            for(int j=i;j<size;j++){
-
+            for(int j=i;j<array.length;j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
             }
+            temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
         }
+
+        return array;
     }
 }
