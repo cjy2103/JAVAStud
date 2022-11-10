0bf13927b2dd0d6b7ec7332cbe4fe412c3b6a48a
jy.choi <cjy2103@naver.com>
2022-11-10 17:04:56 +0900

    개선된 swich/case 추가 설명

--- blogPosting/Conditional/switch/src/Main.java
+++ blogPosting/Conditional/switch/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Test test = new Test();
-        test.test(90);
+        test.test(40);
 
     }
 }
--- blogPosting/Conditional/switch/src/Test.java
+++ blogPosting/Conditional/switch/src/Test.java
@@ -1,23 +1,28 @@
 public class Test {
 
     public void test(int grade){
-//        switch (grade/10){
-//            case 9 : System.out.println("학점은 A");
-//                break;
-//            case 8 : System.out.println("학점은 B");
-//                break;
-//            case 7 : System.out.println("학점은 C");
-//                break;
-//            case 6 : System.out.println("학점은 D");
-//                break;
-//            default: System.out.println("학점은 F");
-//                break;
-//        }
+        switch (grade/10){
+            case 9 : System.out.println("학점은 A");
+                break;
+            case 8 : System.out.println("학점은 B");
+                break;
+            case 7 : System.out.println("학점은 C");
+                break;
+            case 6 : System.out.println("학점은 D");
+                break;
+            case 5 :
+            case 4 :
+            case 3 : System.out.println("학점 D-");
+                break;
+            default: System.out.println("학점은 F");
+                break;
+        }
         switch (grade / 10) {
             case 9 -> System.out.println("학점은 A");
             case 8 -> System.out.println("학점은 B");
             case 7 -> System.out.println("학점은 C");
             case 6 -> System.out.println("학점은 D");
+            case 3,4,5 -> System.out.println("학점 D-");
             default -> System.out.println("학점은 F");
         }
     }
