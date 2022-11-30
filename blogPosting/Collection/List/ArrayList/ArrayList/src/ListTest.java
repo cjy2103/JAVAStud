68a32fdacabf8992885702c603d545cf7f16652b
jy.choi <cjy2103@naver.com>
2022-11-30 17:08:44 +0900

    ArrayList 수정 추가

--- blogPosting/Collection/List/ArrayList/ArrayList/src/ListTest.java
+++ blogPosting/Collection/List/ArrayList/ArrayList/src/ListTest.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class ListTest {
 
     public void listTest(){
-        ArrayList arrayList = new ArrayList();
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
         arrayList.add(1);
         arrayList.add(2);
         arrayList.add(3);
+
+        arrayList.set(2,5);
 
         System.out.println(arrayList);
 
--- blogPosting/Collection/List/ArrayList/ArrayList/src/Main.java
+++ blogPosting/Collection/List/ArrayList/ArrayList/src/Main.java
@@ -3,23 +3,26 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//
+//        arrayList.add(1);
+//        arrayList.add(2);
+//        arrayList.add(3);
+//        arrayList.add(10);
+//        arrayList.add(6);
+//        arrayList.add(5);
+//
+//        System.out.println(arrayList);
+//
+//        arrayList.sort(Comparator.naturalOrder());
+//
+//        System.out.println(arrayList);
+//
+//        arrayList.sort(Comparator.reverseOrder());
+//
+//        System.out.println(arrayList);
+        ListTest listTest = new ListTest();
+        listTest.listTest();
 
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(10);
-        arrayList.add(6);
-        arrayList.add(5);
-
-        System.out.println(arrayList);
-
-        arrayList.sort(Comparator.naturalOrder());
-
-        System.out.println(arrayList);
-
-        arrayList.sort(Comparator.reverseOrder());
-
-        System.out.println(arrayList);
     }
 }
