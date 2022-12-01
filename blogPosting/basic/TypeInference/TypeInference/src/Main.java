cd5ae46e08a1792bfa67627ad1711af8d322c053
jy.choi <cjy2103@naver.com>
2022-12-01 18:01:07 +0900

    Var(타입추론) 포스팅 중

--- blogPosting/basic/TypeInference/TypeInference/src/Main.java
+++ blogPosting/basic/TypeInference/TypeInference/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        
+        var str = "Hello";
+
+        Test test = new Test();
+
+        test.stringType("Hello");
+        test.intType(20);
+        test.floatType(3);
     }
 }
--- blogPosting/basic/TypeInference/TypeInference/src/Test.java
+++ blogPosting/basic/TypeInference/TypeInference/src/Test.java
@@ -1,3 +1,24 @@
 public class Test {
-    
+
+    public void stringType(String compare) {
+        var word    = "Hello";
+
+        if(word.equals(compare)){
+            System.out.println("값은 같다.");
+        } else {
+            System.out.println("값은 다르다..");
+        }
+    }
+
+    public void intType(int number) {
+        var score = 40;
+
+        System.out.println("총합:"+(score+number));
+
+    }
+
+    public void floatType(float number) {
+        var num = 4.5;
+        System.out.println("몫:"+(num/number));
+    }
 }
