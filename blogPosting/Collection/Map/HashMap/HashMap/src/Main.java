ba39e4cb3abf2ffac7eb37360c7233b6743ffee8
jy.choi <cjy2103@naver.com>
2022-12-06 12:10:33 +0900

    HashMap 포스팅 완

--- blogPosting/Collection/Map/HashMap/HashMap/src/Main.java
+++ blogPosting/Collection/Map/HashMap/HashMap/src/Main.java
@@ -1,5 +1,56 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        hashMap.put("Car","자동차");
+        hashMap.put("People","사람");
+        hashMap.put("Book","책");
+
+//        // Map의  Entry 이용
+//        for(Map.Entry<String, String> entry :hashMap.entrySet()){
+//            System.out.printf(
+//                    """
+//                    Key: %s, value : %s
+//                    """, entry.getKey(), entry.getValue());}
+//
+//        System.out.println();
+//
+//        //keySet 이용
+//        for(String key : hashMap.keySet()){
+//            System.out.printf(
+//                    """
+//                    key : %s, value : %s
+//                    """, key, hashMap.get(key)
+//            );
+//        }
+        Iterator<Map.Entry<String,String>> entryIterator
+                                           = hashMap.entrySet().iterator();
+
+        while (entryIterator.hasNext()){
+            Map.Entry<String, String> entry = entryIterator.next();
+            System.out.printf(
+                    """
+                    key: %s, value : %s
+                    """, entry.getKey(), entry.getValue()
+            );
+        }
+
+        System.out.println();
+
+        Iterator<String> keys = hashMap.keySet().iterator();
+
+        while (keys.hasNext()){
+            String key = keys.next();
+            System.out.printf(
+                    """
+                    key: %s, value : %s
+                    """, key, hashMap.get(key)
+            );
+        }
     }
 }
