a52bdf817e7dbc699222c6e41ddf382efdd2c50f
jy.choi <cjy2103@naver.com>
2023-01-03 14:41:46 +0900

    객체 타입 찾아내기 포스팅 완

--- blogPosting/basic/TypeFind/src/Main.java
+++ blogPosting/basic/TypeFind/src/Main.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
+    public static void main(String[] args) {
+        String str = "123456";
+        Integer i = 1;
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3));
+
+        HashMap<String,String> hashMap = new HashMap<>(){{put("Book","Java기초");}};
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("key", 12345);
+
+        System.out.println(str.getClass().getTypeName());
+        System.out.println(i.getClass().getTypeName());
+        System.out.println(list.getClass().getTypeName());
+        System.out.println(hashMap.getClass().getTypeName());
+        System.out.println(map.getClass().getTypeName());
+
+
+    }
 }
