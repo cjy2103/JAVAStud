c5757377bb8a6329373d63868feb7ef29501e067
jy.choi <cjy2103@naver.com>
2023-01-04 11:11:05 +0900

    이중괄호 초기화 포스팅 완

--- blogPosting/basic/DoubleBrace/DoubleBrace/src/Main.java
+++ blogPosting/basic/DoubleBrace/DoubleBrace/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> arrayList = new ArrayList<>(){{
+            add(1);
+            add(2);
+            add(3);
+        }};
+        HashSet<String> country = new HashSet<>(){
+            {
+                add("한국");
+                add("미국");
+                add("일본");
+                add("중국");
+                add("독일");
+            }
+        };
+        HashMap<String,String> student = new HashMap<>(){
+            {
+                put("철수","컴퓨터공학과");
+                put("영희","경제학과");
+                put("길동","건축학과");
+            }
+        };
+
+        System.out.println(arrayList);
+        System.out.println(country);
+        System.out.println(student);
+
+
     }
 }
