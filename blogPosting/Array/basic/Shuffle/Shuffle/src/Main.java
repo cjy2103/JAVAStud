4736369eda4f6ccc3cb617b05b825275bc2e019b
jy.choi <cjy2103@naver.com>
2023-01-07 15:00:46 +0900

    자바 셔플 포스팅 완

--- blogPosting/Array/basic/Shuffle/Shuffle/src/Main.java
+++ blogPosting/Array/basic/Shuffle/Shuffle/src/Main.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayShuffle arrayShuffle = new ArrayShuffle();
-        int[] array = {1,2,3,4,5};
+//        ArrayShuffle arrayShuffle = new ArrayShuffle();
+//        int[] array = {1,2,3,4,5};
+//
+//        array = arrayShuffle.randomUse(array);
+//
+//        System.out.println(Arrays.toString(array));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
 
-        array = arrayShuffle.randomUse(array);
+        Collections.shuffle(arrayList);
 
-        System.out.println(Arrays.toString(array));
+        System.out.println(arrayList);
+
     }
 }
