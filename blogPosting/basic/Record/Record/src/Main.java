5b734e929d825e81bbedc9988177d58e8da94d9c
jy.choi <cjy2103@naver.com>
2023-01-12 10:15:41 +0900

    자바 Record 포스팅 완

--- blogPosting/basic/Record/Record/src/Main.java
+++ blogPosting/basic/Record/Record/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("철수",20);
+        System.out.println(person);
+        System.out.println(person.name());
+        System.out.println(person.age());
+
     }
 }
