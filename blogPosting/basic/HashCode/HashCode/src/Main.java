61ecb7e36643527f634f69b416105a396a044f34
jy.choi <cjy2103@naver.com>
2023-01-13 11:18:15 +0900

    해시코드 포스팅 완

--- blogPosting/basic/HashCode/HashCode/src/Main.java
+++ blogPosting/basic/HashCode/HashCode/src/Main.java
@@ -2,6 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person("철수",25);
+        Person person2 = new Person("영희",22);
 
+        System.out.println(person.hashCode());
+        System.out.println(person2.hashCode());
+
+        System.out.println(person.equals(person2));
     }
 }
--- blogPosting/basic/HashCode/HashCode/src/Person.java
+++ blogPosting/basic/HashCode/HashCode/src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private final String name;
     private final int age;
@@ -16,5 +18,16 @@ public class Person {
         return age;
     }
 
-  Object
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
