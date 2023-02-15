26b9e57daf1cabf543f228a2b1358df14fadff95
jy.choi <cjy2103@naver.com>
2023-02-15 16:39:32 +0900

    자바 라벨링 포스팅 완

--- blogPosting/Loop/Labeling/Labeling/src/Test.java
+++ blogPosting/Loop/Labeling/Labeling/src/Test.java
@@ -20,14 +20,17 @@ public class Test {
     }
 
     public void labelingLoop(){
-        outer : for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                System.out.println(i);
+        outer : for(int i=0;i<5;i++){
+            inner : for(int j=0;j<5;j++){
                 if(i == 2 && j == 2) {
-                    System.out.println(j);
                     break outer;
                 }
+                System.out.printf(
+                        """
+                        i의 값 : %d, j의 값 : %d
+                        """, i, j);
             }
         }
+        System.out.println("Loop 종료");
     }
 }
