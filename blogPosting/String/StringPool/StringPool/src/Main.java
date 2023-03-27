cc69349815dc8a67747940daead9f704c8c34846
jy.choi <cjy2103@naver.com>
2023-03-27 14:50:04 +0900

    String Pool 값 등록, intern

--- blogPosting/String/StringPool/StringPool/src/Main.java
+++ blogPosting/String/StringPool/StringPool/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "Hello";
+        String str2 = new String("Hello").intern();
+
+        if(str1 == str2){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
