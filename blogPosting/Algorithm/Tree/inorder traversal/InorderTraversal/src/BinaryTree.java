2bba5f96675eb8c68a04019ffc128f1b39f266d5
jy.choi <cjy2103@naver.com>
2023-04-06 14:24:09 +0900

    중위순회

--- blogPosting/Algorithm/Tree/inorder traversal/InorderTraversal/src/BinaryTree.java
+++ blogPosting/Algorithm/Tree/inorder traversal/InorderTraversal/src/BinaryTree.java
@@ -1,3 +1,28 @@
 public class BinaryTree {
-    
+    Node root;
+
+    public void insert(int data){
+        root = insertNode(root, data);
+    }
+
+    private Node insertNode(Node current, int data){
+        if(current == null){
+            return new Node(data);
+        }
+
+        if(data < current.data){
+            current.left = insertNode(current.left, data);
+        } else if(data > current.data){
+            current.right = insertNode(current.right, data);
+        }
+
+        return current;
+    }
+
+    /**
+     * @DESC: 중위 순회
+     */
+    public void inOrderTraversal(Node root){
+
+    }
 }
