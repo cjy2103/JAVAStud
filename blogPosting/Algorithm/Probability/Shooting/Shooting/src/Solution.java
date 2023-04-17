2e5de34c2103590d7384512f5cf34fa17a4aa7eb
jy.choi <cjy2103@naver.com>
2023-04-17 13:22:05 +0900

    수식 오류 수정

--- blogPosting/Algorithm/Probability/Shooting/Shooting/src/Solution.java
+++ blogPosting/Algorithm/Probability/Shooting/Shooting/src/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
         double targetProbability = 0.9;
         double currentProbability = 0.0; // 현재까지의 적중 확률 초기화
 
-        while (currentProbability <= targetProbability) {
+        while (currentProbability < targetProbability) {
             answer++; // 총알 발사 횟수 증가
             currentProbability = 1 - Math.pow(1 - hitProbability, answer); // 현재까지의 적중 확률 업데이트
         }
