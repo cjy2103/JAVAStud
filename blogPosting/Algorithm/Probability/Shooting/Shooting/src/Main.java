96dd01499b61ec0da9c3220c543678101bf74526
jy.choi <cjy2103@naver.com>
2023-04-17 13:08:34 +0900

    no message

--- blogPosting/Algorithm/Probability/Shooting/Shooting/src/Main.java
+++ blogPosting/Algorithm/Probability/Shooting/Shooting/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double hitProbability = 0.1;
+        double hitProbability = 0.7;
         Solution solution = new Solution();
         int answer = solution.solution(hitProbability);
         System.out.println(answer);
