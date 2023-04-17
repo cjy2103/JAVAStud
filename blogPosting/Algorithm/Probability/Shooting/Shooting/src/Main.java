c69258d02e388e0e688f1bd52238b1095d9928a5
jy.choi <cjy2103@naver.com>
2023-04-17 12:24:47 +0900

    베르누이 확률분포, 누적분포함수 자바 구현 포스팅 완

---
+++ blogPosting/Algorithm/Probability/Shooting/Shooting/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double percent = 0.7;
+        Solution solution = new Solution();
+        int answer = solution.solution(percent);
+        System.out.println(answer);
+    }
+}
---
+++ blogPosting/Algorithm/Probability/Shooting/Shooting/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public int solution(double hitProbability){
+        int answer = 0; // 총알 발사 횟수 초기화
+        double targetProbability = 0.9;
+        double currentProbability = 0.0; // 현재까지의 적중 확률 초기화
+
+        while (currentProbability <= targetProbability) {
+            answer++; // 총알 발사 횟수 증가
+            currentProbability = 1 - Math.pow(1 - hitProbability, answer); // 현재까지의 적중 확률 업데이트
+        }
+
+        return answer;
+    }
+
+}
