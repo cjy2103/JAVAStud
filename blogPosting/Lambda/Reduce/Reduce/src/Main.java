f9e5bd4bbcbeba8e4c3a3df558648fa2d290f27e
jy.choi <cjy2103@naver.com>
2023-04-27 23:11:13 +0900

    reduce 메서드 포스팅 완

--- blogPosting/Lambda/Reduce/Reduce/src/Main.java
+++ blogPosting/Lambda/Reduce/Reduce/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+
+        int sum = list.stream().reduce(0, (a,b) -> a+b);
+        System.out.println(sum);
     }
 }
