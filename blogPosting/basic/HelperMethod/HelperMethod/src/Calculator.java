a28e79a0cb16bc586370cb9087e575258027db99
jy.choi <cjy2103@naver.com>
2023-05-02 11:15:06 +0900

    헬퍼 메서드 포스팅 완

---
+++ blogPosting/basic/HelperMethod/HelperMethod/src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public void test(){
+        int sum = add(2,3);
+        System.out.println(sum);
+    }
+
+    private int add(int a, int b){
+        return a+b;
+    }
+}
