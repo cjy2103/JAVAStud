6ad64ffaf0e25fc2ba31a471c91602eeadadcae9
jy.choi <cjy2103@naver.com>
2023-07-02 04:30:11 +0900

    이분탐색 포스팅 완

--- blogPosting/Algorithm/BinarySearch/BinarySearch/src/BinarySearch.java
+++ blogPosting/Algorithm/BinarySearch/BinarySearch/src/BinarySearch.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
 public class BinarySearch {
     public int search(int[] arr, int target){
 
-        
+        Arrays.sort(arr);
 
-        return 0;
+        int left = 0;
+        int right = arr.length-1;
+        int count = 0;
+
+        while (left <= right){
+            int mid = (left + right) / 2;
+            count++;
+
+            if(arr[mid] == target){
+                return count;
+            } else if(arr[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // 타겟이 배열에 존재하지 않을 경우 -1 반환
     }
 }
--- blogPosting/Algorithm/BinarySearch/BinarySearch/src/Main.java
+++ blogPosting/Algorithm/BinarySearch/BinarySearch/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int [] numbers = {1,7,59,3,19,47,17,11,23,41,53,37,29,43,13,31,5};
+        int target = 37;
+        BinarySearch binarySearch = new BinarySearch();
+        int count = binarySearch.search(numbers,target);
+
+        System.out.println("37을 찾기 위해 이분 탐색을 " + count + "번 수행했습니다.");
 
     }
 }
