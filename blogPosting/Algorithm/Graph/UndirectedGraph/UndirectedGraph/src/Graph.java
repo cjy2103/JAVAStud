7ca6cede6d4b123d698a63a30888bfc506a98e48
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-07-10 16:06:43 +0900

    무방향 그래프 포스팅 완

--- blogPosting/Algorithm/Graph/UndirectedGraph/UndirectedGraph/src/Graph.java
+++ blogPosting/Algorithm/Graph/UndirectedGraph/UndirectedGraph/src/Graph.java
@@ -1,2 +1,31 @@
+import java.util.LinkedList;
+
 public class Graph {
+    public int v; // 노드의 개수
+    private LinkedList<Integer>[] adj; // 인접리스트
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i<v;i++){
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int v, int w){
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public void dfs(int v, boolean[] visited){
+        visited[v] = true;
+        System.out.println(v+ " ");
+
+        for (int n : adj[v]) {
+            if (!visited[n]) {
+                dfs(n, visited);
+            }
+        }
+    }
+
 }
--- blogPosting/Algorithm/Graph/UndirectedGraph/UndirectedGraph/src/Main.java
+++ blogPosting/Algorithm/Graph/UndirectedGraph/UndirectedGraph/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Graph graph = new Graph(5); // 노드의 개수가 5인 무방향 그래프 생성
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+
+        boolean[] visited = new boolean[5];
+        graph.dfs(0,visited);
     }
 }
