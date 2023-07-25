b191d47f77a4daafc3e62410c0b88919124bc640
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-07-25 10:56:24 +0900

    SRP 미적용

--- blogPosting/etc/SRPNotApplied/srpnotapplied/src/Car.java
+++ blogPosting/etc/SRPNotApplied/srpnotapplied/src/Car.java
@@ -1,3 +1,25 @@
 public class Car {
-    
+    private String model;
+    private String color;
+    private double price;
+
+    public Car(String model, String color, double price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public void printCarInfo(){
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Price: " + price);
+    }
+
+    public void drive(){
+        System.out.println("차량 운전중....");
+    }
+
+    public void repair(){
+        System.out.println("차량 수리중...");
+    }
 }
--- blogPosting/etc/SRPNotApplied/srpnotapplied/src/Main.java
+++ blogPosting/etc/SRPNotApplied/srpnotapplied/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("테슬라 모델 3", "RED", 6000);
+
+        car.printCarInfo();
+
+        car.drive();
+
+        car.repair();
     }
 }
