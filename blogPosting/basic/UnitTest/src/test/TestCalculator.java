2644cd18d6ff249b48d1d70f2de2f595cbdcdf88
S120916\최재영 <cjy2103@naver.com>
2024-01-04 15:55:36 +0900

    자바(Java) - JUnit 오류 - JUnit package org.junit.jupiter.api does not exist ( )

--- blogPosting/basic/UnitTest/src/test/TestCalculator.java
+++ blogPosting/basic/UnitTest/src/test/TestCalculator.java
@@ -1,8 +1,7 @@
 import main.Calculator;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestCalculator {
