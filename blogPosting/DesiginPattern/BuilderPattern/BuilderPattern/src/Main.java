f245a4fab9f8b81d204d49e80f0574cb4469bb06
S120916\최재영 <cjy2103@naver.com>
2024-01-09 09:02:35 +0900

    자바(JAVA) 디자인 패턴 - 빌더(Builder) 패턴 ( v )

--- blogPosting/DesiginPattern/BuilderPattern/BuilderPattern/src/Main.java
+++ blogPosting/DesiginPattern/BuilderPattern/BuilderPattern/src/Main.java
@@ -1,5 +1,14 @@
+//클라이언트 (Director)
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 빌더를 사용하여 객체 생성
+        Person person = new Person.PersonBuilder()
+                .withName("철수")
+                .withAge(30)
+                .withAddress("부산광역시 남구")
+                .build();
+
+        // 생성된 객체 출력
+        System.out.println(person);
     }
 }
--- blogPosting/DesiginPattern/BuilderPattern/BuilderPattern/src/Person.java
+++ blogPosting/DesiginPattern/BuilderPattern/BuilderPattern/src/Person.java
@@ -3,4 +3,66 @@ public class Person {
     private String name;
     private int age;
     private String address;
+
+    // 생성자는 private로 선언하여 외부에서 직접 생성하지 못하도록 합니다.
+    private Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    // PersonBuilder 클래스를 내부에 정의
+    public static class PersonBuilder {
+        private Person person = new Person();
+
+        public PersonBuilder withName(String name){
+            person.name = name;
+            return this;
+        }
+
+        public PersonBuilder withAge(int age) {
+            person.age = age;
+            return this;
+        }
+
+        public PersonBuilder withAddress(String address) {
+            person.address = address;
+            return this;
+        }
+
+        public Person build() {
+            return person;
+        }
+    }
 }
