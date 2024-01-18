b98d35349b23fe5323dcf537be4f1c7b12dafefb
S120916\최재영 <cjy2103@naver.com>
2024-01-18 17:51:46 +0900

    검증중

--- blogPosting/Algorithm/FindMissingNum/MissingNumber/src/Main.java
+++ blogPosting/Algorithm/FindMissingNum/MissingNumber/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[100000000];
+        for (int i = 0; i < 54321; i++) {
+            arr[i] = i + 1;
+        }
+        for (int i = 54322; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        int result = findMissingNumber(arr);
+        System.out.println("빠진 숫자: " + result);
+    }
+
+
+    static int findMissingNumber(int[] arr){
+        int result = 0;
+
+        // 모든 배열 요소와 인덱스에 대해 XOR 연산을 수행
+        for (int i = 0; i < arr.length; i++) {
+            result ^= i ^ arr[i];
+        }
+
+        // 배열의 길이만큼 XOR 연산을 수행하여 빠진 값 찾기
+        result ^= arr.length;
+
+        return result;
     }
 }
