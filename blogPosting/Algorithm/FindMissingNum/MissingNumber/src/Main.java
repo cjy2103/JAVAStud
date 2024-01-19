8f934a937547d478b0ae63cad1a42a34ea4aa4e9
S120916\최재영 <cjy2103@naver.com>
2024-01-19 16:11:28 +0900

    구현 완

--- blogPosting/Algorithm/FindMissingNum/MissingNumber/src/Main.java
+++ blogPosting/Algorithm/FindMissingNum/MissingNumber/src/Main.java
@@ -1,28 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[100000000];
-        for (int i = 0; i < 54321; i++) {
-            arr[i] = i + 1;
-        }
-        for (int i = 54322; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        int result = findMissingNumber(arr);
-        System.out.println("빠진 숫자: " + result);
+        int[] numbers = {4, 10, 1, 6, 7, 5, 8 ,2, 9,11,13,12,14,15};
+        int missingNumber = findMissingNumber(numbers);
+        System.out.println("빠진 숫자는: " + missingNumber);
     }
-
-
     static int findMissingNumber(int[] arr){
-        int result = 0;
+        int n = arr.length + 1;
+        int xorResult = 0;
 
-        // 모든 배열 요소와 인덱스에 대해 XOR 연산을 수행
-        for (int i = 0; i < arr.length; i++) {
-            result ^= i ^ arr[i];
+        // 1부터 n까지의 숫자를 XOR 연산
+        for (int i = 1; i <= n; i++) {
+            xorResult ^= i;
         }
 
-        // 배열의 길이만큼 XOR 연산을 수행하여 빠진 값 찾기
-        result ^= arr.length;
+        // 주어진 숫자들에 대해서도 XOR 연산
+        for (int num : arr) {
+            xorResult ^= num;
+        }
 
-        return result;
+        return xorResult;
     }
 }
